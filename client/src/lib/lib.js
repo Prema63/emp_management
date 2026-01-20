6292744e6ffd0c60b200faa42608c1a90baebed0
Prema63 <darkshadowprema@gmail.xom>
2026-01-20 11:46:26 +0530

    Ready to deploy

--- client/src/lib/lib.js
+++ client/src/lib/lib.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="http://localhost:8000/";
-export const BASE_URL="https://employee-management-gf0o.onrender.com/";
+export const BASE_URL="https://emp-management-8slb.onrender.com/";
 
     
